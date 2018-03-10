b9364196bc1eaac4d58bffe48ff1dd7c65dfd4f0
Alisan Dagdelen <alisandagdelen@gmail.com>
2018-03-10 19:08:31 +0300

    - WebService implementation

--- BusSchedule/Utility/Services/WebService.swift
+++ BusSchedule/Utility/Services/WebService.swift
@@ -7,3 +7,45 @@
 //
 
 import Foundation
+import Alamofire
+import AlamofireObjectMapper
+
+typealias GenericObjectClosure<T> = (_ object:T?, _ error: Error?)-> Void
+
+class WebService{
+    
+    static let sharedInstance = WebService()
+    fileprivate init() {}
+    
+    private func createRequest(method:Alamofire.HTTPMethod, path:String, params: [String : Any]?) -> URLRequest {
+        let URL = Foundation.URL(string:ApiURL.baseURL + path)!
+        
+        var req = URLRequest(url:URL)
+        req.httpMethod = method.rawValue
+        req.addValue(ApiHeader.authHeaderValue, forHTTPHeaderField: ApiHeader.authHeaderKey)
+        
+        do {
+            return try Alamofire.JSONEncoding().encode(req, with:params)
+        }
+        catch{
+            return req
+        }
+    }
+    
+    private func callRequest(_ req:URLRequest) -> DataRequest {
+        return Alamofire.request(req).validate()
+    }
+    
+    // MARK: REST methods
+    
+    func getObject<T:BaseModel>( pathUrl: String, _ result:@escaping GenericObjectClosure<T>) {
+        
+        callRequest(createRequest(method: .get, path: pathUrl, params: nil)).responseObject { (response:DataResponse<T>) -> Void in
+            if let object = response.result.value {
+                result(object, nil)
+            } else {
+                result(nil, response.result.error)
+            }
+        }
+    }
+}
