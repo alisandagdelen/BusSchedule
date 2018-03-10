36af9d1d5c32e38eebe8c8d1046a49a7279ec045
Alisan Dagdelen <alisandagdelen@gmail.com>
2018-03-11 00:36:38 +0300

    - TimeTableVC implementations

--- BusSchedule/Controllers/TimeTableVC.swift
+++ BusSchedule/Controllers/TimeTableVC.swift
@@ -9,10 +9,52 @@
 import UIKit
 
 class TimeTableVC: UIViewController {
-
+    
+    @IBOutlet weak var tblTimeTableDetails: UITableView!
     var timeTableViewModel:TimeTableViewModelProtocol!
+    
+    private var dataSource: TableViewDataSourceWithSection<TCellTimeTableDetails, String, TimeTableDetails>!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupUI()
+        fillUI()
+    }
+    
+    func setupUI() {
+        self.navigationItem.title = timeTableViewModel.stationName
+        self.navigationController?.navigationBar.tintColor = UIColor.white
+        let rightBarButtonImage = timeTableViewModel.selectedDateType.value == .departure ? #imageLiteral(resourceName: "departure") : #imageLiteral(resourceName: "arrival")
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(image: rightBarButtonImage, style: UIBarButtonItemStyle.plain, target: self, action: #selector(changeDateType(_:)))
+        
+        tblTimeTableDetails.register(UINib(nibName: TCellTimeTableDetails.nibName, bundle: nil), forCellReuseIdentifier: TCellTimeTableDetails.nibName)
+        tblTimeTableDetails.tableFooterView = UIView.init(frame: CGRect.zero)
+        print(timeTableViewModel.selectedDateType.value)
+    }
+    
+    func fillUI() {
+        
+        timeTableViewModel.timeTableDetails.bind { [unowned self] timeTableDetails in
+            let dates = self.timeTableViewModel.dates
+            self.dataSource = TableViewDataSourceWithSection<TCellTimeTableDetails, String, TimeTableDetails>(cellIdentifier: TCellTimeTableDetails.nibName, sections: dates, items: timeTableDetails, configureCell
+                : { (cell, timeTableDetails) in
+                    cell.lblHour.text = timeTableDetails.time?.hourFromTimeStampWithTimeZone
+                    cell.lblRoute.text = timeTableDetails.briefRoute
+                    cell.lblDirection.text = "Line \(timeTableDetails.lineNumber) direction \(timeTableDetails.direction)"
+            }, configureSection: { (titleLabel:UILabel, date:String) in
+                titleLabel.text = date
+            })
+            self.tblTimeTableDetails.dataSource = self.dataSource
+            self.tblTimeTableDetails.reloadData()
+        }
+        
+        timeTableViewModel.selectedDateType.bind { [unowned self] in
+            self.navigationItem.rightBarButtonItem?.image = $0 == .departure ? #imageLiteral(resourceName: "departure") : #imageLiteral(resourceName: "arrival")
+            print($0)
+        }
+    }
+    
+    @objc func changeDateType(_ sender:UIButton) {
+        timeTableViewModel.changeDateType()
     }
 }
