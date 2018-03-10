f12cc35a1e569b5d5b9695c35007239b94c589b4
Alisan Dagdelen <alisandagdelen@gmail.com>
2018-03-10 19:07:46 +0300

    - NibName extension added - FromNib extension added

--- BusSchedule/Utility/Extensions/UIViewExtension.swift
+++ BusSchedule/Utility/Extensions/UIViewExtension.swift
@@ -6,4 +6,17 @@
 //  Copyright © 2018 alisandagdelen. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+extension UIView {
+    
+    static var nibName: String {
+        return String(describing: self)
+    }
+    
+    static var fromNib: UIView? {
+        let nib = UINib(nibName:nibName, bundle:nil)
+        let view = nib.instantiate(withOwner: nil, options: nil).first as? UIView
+        return view
+    }
+}
