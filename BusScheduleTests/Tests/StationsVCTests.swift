f59dca8d3d8f5332c773f23860d90499678f5322
Alisan Dagdelen <alisandagdelen@gmail.com>
2018-03-11 22:41:43 +0300

    - StationsVC test implemented

---
+++ BusScheduleTests/Tests/StationsVCTests.swift
@@ -0,0 +1,71 @@
+//
+//  StationsVCTests.swift
+//  BusScheduleTests
+//
+//  Created by alisandagdelen on 11.03.2018.
+//  Copyright © 2018 alisandagdelen. All rights reserved.
+//
+
+import XCTest
+@testable import BusSchedule
+
+class StationsVCTests: XCTestCase {
+    
+    var stationsVC: StationsVC!
+    var stationsViewModel:StationsViewModelProtocol!
+    
+    override func setUp() {
+        super.setUp()
+        
+        let storyboard = UIStoryboard(name: "Main",
+                                      bundle: Bundle.main)
+        let navigationController = storyboard.instantiateInitialViewController() as! UINavigationController
+        stationsVC = navigationController.topViewController as! StationsVC
+        stationsViewModel = StationsViewModelMock()
+        stationsVC.stationsViewModel = StationsViewModelMock()
+        UIApplication.shared.keyWindow!.rootViewController = stationsVC
+        _ = navigationController.view
+        _ = stationsVC.view
+    }
+    
+    func testPresentTimeTableVC() {
+        stationsVC.presentTimeTableVC(city: .berlin, dataService: BusServiceMock())
+        XCTAssertEqual(stationsVC.navigationController!.topViewController!, stationsVC)
+    }
+    
+    func testNavigationTitle() {
+        XCTAssertEqual(stationsVC.navigationItem.title, "Stations")
+    }
+    
+    func testHasATableView() {
+        XCTAssertNotNil(stationsVC.tblStations)
+    }
+    
+    func testDidSelectRow() {
+        let indexPath = IndexPath(row: 1, section: 0)
+        stationsVC.tableView(stationsVC.tblStations, didSelectRowAt: indexPath)
+        XCTAssertEqual(stationsVC.navigationController!.topViewController!, stationsVC)
+    }
+    
+    func testTableViewHasDelegate() {
+        XCTAssertNotNil(stationsVC.tblStations.delegate)
+    }
+    
+    func testTableViewConfromsToTableViewDelegateProtocol() {
+        XCTAssertTrue(stationsVC.conforms(to: UITableViewDelegate.self))
+        XCTAssertTrue(stationsVC.responds(to: #selector(stationsVC.tableView(_:didSelectRowAt:))))
+    }
+    
+    func testTableViewHasDataSource() {
+        XCTAssertNotNil(stationsVC.tblStations.dataSource)
+    }
+    
+    func testTableViewSectionSet() {
+        XCTAssertEqual(stationsVC.tblStations.numberOfSections, stationsViewModel.countries.count)
+    }
+    
+    func testTableViewRowSet() {
+        XCTAssertEqual(stationsVC.tblStations.numberOfRows(inSection: 0), stationsViewModel.stations[stationsViewModel.countries[0]]?.count)
+    }
+    
+}
