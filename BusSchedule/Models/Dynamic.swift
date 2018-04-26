7af5bff713d1c8496896132da626880f5ee0134b
Alisan Dagdelen <alisandagdelen@gmail.com>
2018-04-27 00:21:16 +0300

    - Dynamic class commented

--- BusSchedule/Models/Dynamic.swift
+++ BusSchedule/Models/Dynamic.swift
@@ -8,22 +8,23 @@
 
 import Foundation
 
-class Dynamic<T> {
-    typealias Listener = (T) -> ()
-    var listener: Listener?
-    
-    var value: T {
-        didSet {
-            listener?(value)
-        }
-    }
-    
-    func bind(_ listener: Listener?) {
-        self.listener = listener
-        listener?(value)
-    }
-    
-    init(_ value: T) {
-        self.value = value
-    }
-}
+//class Dynamic<T> {
+//    typealias Listener = (T) -> ()
+//    var listener: Listener?
+//
+//    var value: T {
+//        didSet {
+//            listener?(value)
+//        }
+//    }
+//
+//    func bind(_ listener: Listener?) {
+//        self.listener = listener
+//        listener?(value)
+//    }
+//
+//    init(_ value: T) {
+//        self.value = value
+//    }
+//}
+
