662474292ed8d8257adbe36f8aaefe9b78294727
Alisan Dagdelen <alisandagdelen@gmail.com>
2018-04-27 00:21:36 +0300

    - Codable extension

--- BusSchedule/Utility/Services/WebService.swift
+++ BusSchedule/Utility/Services/WebService.swift
@@ -8,7 +8,6 @@
 
 import Foundation
 import Alamofire
-import AlamofireObjectMapper
 
 typealias GenericObjectClosure<T> = (_ object:T?, _ error: Error?)-> Void
 
@@ -19,7 +18,6 @@ class WebService{
     
     private func createRequest(method:Alamofire.HTTPMethod, path:String, params: [String : Any]?) -> URLRequest {
         let URL = Foundation.URL(string:ApiURL.baseURL + path)!
-        
         var req = URLRequest(url:URL)
         req.httpMethod = method.rawValue
         req.addValue(ApiHeader.authHeaderValue, forHTTPHeaderField: ApiHeader.authHeaderKey)
@@ -39,8 +37,8 @@ class WebService{
     // MARK: REST methods
     
     func getObject<T:BaseModel>( pathUrl: String, _ result:@escaping GenericObjectClosure<T>) {
-        
         callRequest(createRequest(method: .get, path: pathUrl, params: nil)).responseObject { (response:DataResponse<T>) -> Void in
+            
             if let object = response.result.value {
                 result(object, nil)
             } else {
@@ -49,3 +47,29 @@ class WebService{
         }
     }
 }
+
+
+extension DataRequest {
+    fileprivate func decodableResponseSerializer<T: Decodable>() -> DataResponseSerializer<T> {
+        return DataResponseSerializer { _, response, data, error in
+            guard error == nil else { return .failure(error!) }
+            
+            guard let data = data else {
+                return .failure(AFError.responseSerializationFailed(reason: .inputDataNil))
+            }
+            
+            return Result { try JSONDecoder().decode(T.self, from: data) }
+        }
+    }
+    
+    @discardableResult
+    fileprivate func responseDecodable<T: Decodable>(queue: DispatchQueue? = nil, completionHandler: @escaping (DataResponse<T>) -> Void) -> Self {
+        return response(queue: queue, responseSerializer: decodableResponseSerializer(), completionHandler: completionHandler)
+    }
+    
+    @discardableResult
+    func responseObject<T: Decodable>(queue: DispatchQueue? = nil, completionHandler: @escaping (DataResponse<T>) -> Void) -> Self {
+        return responseDecodable(queue: queue, completionHandler: completionHandler)
+    }
+}
+
